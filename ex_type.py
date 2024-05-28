99e21b056a94a7410d9d14f12c3f41f96f3ea0b7
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-05-28 23:48:36 +0200

    add exs

--- ex_type.py
+++ ex_type.py
@@ -1,12 +1,14 @@
 from refresh_plot import subplots, Refresher, UpdatableBar
+from random import randint
 
-REFRESH_TOUR = 20
+REFRESH_TOUR = 2000
+REFRESH_TEXT_TOUR = 200000
 
 def ex_type_2_bars(name1, name2, evenement):
     r = Refresher(*subplots())
     b1, b2 = UpdatableBar.create_bars(r, name1, name2)
     r.init_elem()
-    tour = 0
+    tour = 1
     while True:
         if evenement():
             b1.value += 1
@@ -15,4 +17,6 @@ def ex_type_2_bars(name1, name2, evenement):
         
         if tour % REFRESH_TOUR == 0:
             r.update()
+        if tour % REFRESH_TEXT_TOUR == 0:
+            print(f"fréquence d'apparition de {name1} : {b1.value / tour} pour {tour} tours. En effet, il y a {b1.value} {name1}")
         tour += 1
--- exercices.py
+++ exercices.py
@@ -2,20 +2,49 @@ from random import randint
 from ex_type import *
 
 
-def ex1():
-    ex_type_2_bars("A", "B", lambda: bool(randint(0, 1)))
+def ex16_1():
+    ex_type_2_bars("Pile", "Face", lambda: bool(randint(0, 1)))
 
-#ex 16 1
-def lance_equilibre():
-    return bool(randint(0, 1))
+def ex16_2():
+    ex_type_2_bars("Pile", "Face", lambda: randint(1, 5) <= 3)
 
-#ex 16 2
-def lance_truque():
-    return randint(1, 5) <= 3
+def ex17():
+    ex_type_2_bars("Noir", "Blanc", lambda: randint(1, 12) <= 5)
 
-#ex 17
-def urne_jetons():
-    return randint(1, 12) <= 5
+def ex18():
+    ex_type_2_bars("Blanc", "Noir", lambda: randint(1, 5) <= 3)
+
+def partie_ex20():
+    points = 0
+    for _ in range(5):
+        points += randint(0, 6)
+        if points >= 20:
+            return True
+    return False
+
+def ex20_bonus():
+    ex_type_2_bars("gagné", "perdu", partie_ex20)
+
+def ex20():
+    gain = 5
+    tour = 1
+    while True:
+        if partie_ex20():
+            gain += 10
+        else:
+            gain -= 5
+        tour += 1
+        if tour % 200000 == 0:
+            print(f"Gains : {gain}, Moyenne par tour : {gain / tour}")
+
+def partie_ex21():
+    for _ in range(6):
+        if randint(0, 6) == 6:
+            return True
+    return False
+
+def ex21():
+    ex_type_2_bars("Lièvre", "Tortue", partie_ex21)
 
 if __name__ == "__main__":
-    ex1()
+    ex21()
